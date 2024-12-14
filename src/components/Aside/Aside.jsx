9a251b370bea689c2888cf39ae14998990bb839e
João Fernandes <joaofernandesxd1@icloud.com>
2024-12-14 12:49:03 -0300

    remove twitter componete aside

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -1,19 +1,14 @@
 import '../Aside/Aside.css';
 import RankingGTA from '../RankingGTA/RankingGTA';
 import Slide from '../Slide/Slide';
-import { TwitterTimelineEmbed} from 'react-twitter-embed';
+
 
 function Aside(){
     return(
         <aside className='aside'>
             <Slide></Slide>
             <RankingGTA></RankingGTA>
-            <TwitterTimelineEmbed
-            sourceType="profile"
-            screenName="RockstarGames"
-            options={{height: 400}}
-            />
-            aqui vai t
+            aqui
         </aside>
     )
 }
--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    target: 'esnext', // ou 'es6' para maior compatibilidade com navegadores modernos
-  },
-  resolve: {
-    alias: {
-      buffer: 'buffer-es6',
-      stream: 'stream-browserify',
-      crypto: 'crypto-browserify',
-      url: 'url',
-      http: 'stream-http',
-      https: 'stream-http',
-    }
-  }
-});
+})
