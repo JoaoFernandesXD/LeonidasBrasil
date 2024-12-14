eb46aeb1b8e669293ad7543810352d1cac30a518
João Fernandes <joaofernandesxd1@icloud.com>
2024-12-13 21:01:51 -0300

    feat: ajustando footer

--- src/components/Foooter/Footer.jsx
+++ src/components/Foooter/Footer.jsx
@@ -9,9 +9,21 @@ function Footer(){
     return(
         <footer>
             <div className="credits">Site desenvolvido por: <strong>João Fernande</strong></div>
-            <FaYoutube />
-            <FaSquareXTwitter />
-            <FaSquareInstagram />
+           <div className='container__footer'>
+            <div className='container__medias_sociais'>
+                <p>Siga-nos em nossas redes sociais!</p>
+                <FaYoutube />
+                <FaSquareXTwitter />
+                <FaSquareInstagram />
+            </div>
+            <p className="copyright">Copyright © Todos os direitos reservados. Leonidas Brasil 2024.</p>
+            <p className="rockstargames">
+            Este portal não possui qualquer vínculo com a Rockstar Games ou suas empresas afiliadas.  
+Trata-se de um projeto totalmente independente, sem patrocínio, apoio ou aprovação oficial por parte da Rockstar Games.  
+Eventuais marcas registradas ou propriedades intelectuais mencionadas pertencem à Rockstar Games e são utilizadas conforme permitido por sua política de fã sites. v1.0
+                 
+            </p>
+           </div>
         </footer>
     )
 }
