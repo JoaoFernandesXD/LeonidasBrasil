681923fc36c33dd86af936af7afddab19cd20823
João Fernandes <joaofernandesxd1@icloud.com>
2024-12-14 18:52:14 -0300

    feat: adicionando tooltips

--- src/components/SingleRankingGTA/SingleRankingGTA.jsx
+++ src/components/SingleRankingGTA/SingleRankingGTA.jsx
@@ -4,6 +4,11 @@ import { FaCalendar } from "react-icons/fa";
 import { IoCloudDownloadSharp } from "react-icons/io5";
 import { FaStop } from "react-icons/fa";
 
+
+/* tippy.js */
+import Tippy from '@tippyjs/react/headless';
+
+
 function SingleRankingGTA(props){
     return(
         <div className='container__single_gta'>
@@ -14,13 +19,13 @@ function SingleRankingGTA(props){
                     <p>{props.name}</p>
                     <div className='info__single__tag'>
                         <div className='container__info__tag'>
-                            <h5><IoCloudDownloadSharp />{props.downloads}</h5>
+                            <Tippy render={attrs => ( <div className="box" tabIndex="-1" {...attrs}> <p>{props.downloads} de vendas</p><div id="arrow" data-popper-arrow></div></div>)}><h5><IoCloudDownloadSharp />{props.downloads}</h5></Tippy>
                         </div>
                         <div className='container__info__tag'>
-                            <h5><FaCalendar />{props.years}</h5>
+                            <Tippy render={attrs => ( <div className="box" tabIndex="-1" {...attrs}> <p>Lançado no ano de {props.years}</p><div id="arrow" data-popper-arrow></div></div>)}><h5><FaCalendar />{props.years}</h5></Tippy>
                         </div>
                         <div className='container__info__tag'>
-                            <h5><FaStop />stable</h5>
+                            <Tippy render={attrs => ( <div className="box" tabIndex="-1" {...attrs}> <p>stable</p><div id="arrow" data-popper-arrow></div></div>)}><h5><FaStop />stable</h5></Tippy>
                         </div>
                     </div>
                 </div>
