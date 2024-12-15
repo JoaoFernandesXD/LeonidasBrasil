d7e7a1c3f51967e967700b46529dea22abf91453
João Fernandes <joaofernandesxd1@icloud.com>
2024-12-15 20:25:14 -0300

    feat: adicionando modal

--- src/components/MainNav/MainNav.jsx
+++ src/components/MainNav/MainNav.jsx
@@ -7,10 +7,16 @@ import { MdPoll } from "react-icons/md";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoLogIn } from "react-icons/io5";
 
+import React, { useState } from 'react';
+import { Modal } from 'react-responsive-modal';
+import 'react-responsive-modal/styles.css'; // Importar o CSS padrão da biblioteca
 
 function MainNav() {
+
+    const [open, setOpen] = React.useState(false);
+
     return(
-        <>
+     
             <div className='main-nav'>
                 <div className='row'>
                 <ul className="d-flex">
@@ -20,11 +26,31 @@ function MainNav() {
                     <a href="#"><li className="nav--item"><PiDevicesFill /> Plataformas </li></a>
                     <a href="#"><li className="nav--item"><MdPoll /> Enquetes</li></a>
                     <a className="trigger--side-nav"><li className="nav--item"><GiHamburgerMenu /> Extras</li></a>
-                    <button className="nav--btn"><IoLogIn /> Login</button>
+                    <button className="nav--btn" onClick={() => setOpen(true)}><IoLogIn /> Login</button>
                 </ul>
                 </div>
-            </div>
-        </>
+                <Modal open={open} onClose={() => setOpen(false)} center classNames={{ overlay: 'customOverlay', modal: 'customModal' }}>
+                    <div className="modal-content">
+                        <h2>Olá, seja bem vindo(a)!</h2>
+                        <form className="login-form">
+                            <div className="form-group">
+                                <label htmlFor="username">Usuário</label>
+                                <input type="text" id="username" name="username" placeholder="Digite seu usuário" required />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="password">Senha</label>
+                                <input type="password" id="password" name="password" placeholder="Digite sua senha" required />
+                            </div>
+                            <button type="submit" className="submit-btn">Entrar</button>
+                        </form>
+                        <div className="form-links">
+                            <a href="#" className="forgot-password">Esqueceu a senha?</a>
+                            <a href="#" className="register-link">Novo usuário? Cadastre-se!</a>
+                        </div>
+                    </div>
+                </Modal>
+
+            </div>      
     )
 }
 
