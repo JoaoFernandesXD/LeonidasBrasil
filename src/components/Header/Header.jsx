33f316137dbf36cda51ab28633b0cbdc52763176
João Fernandes <joaofernandesxd1@icloud.com>
2024-12-15 16:09:28 -0300

    adicionando logado no menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,23 @@
 import '../Header/Header.css';
 
+import logo from '/src/assets/image/logo.png';
 
 function Header(){
     return(
-        <header className='header'></header>
+        <header className='header'>
+            <div className='container__usuario_logado'>
+                 <div className='container__usuario_logado_botao'>
+                    <button class="botao">Botão 1</button>
+                    <button class="botao">Botão 2</button>
+
+                 </div>
+                 <img src={logo}></img>
+                 <div className='container__usuario_logado_botao'>
+                    <button class="botao">Botão 1</button>
+                    <button class="botao">Botão 2</button>
+                 </div>
+            </div> 
+        </header>
     )
 }
 
