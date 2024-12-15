928cbded78f5878855ce24c02ddfbaa90be02971
João Fernandes <joaofernandesxd1@icloud.com>
2024-12-15 19:22:13 -0300

    fix: ajustando bug componete yt

--- src/components/SingleCardYT/SingleCardYT.jsx
+++ src/components/SingleCardYT/SingleCardYT.jsx
@@ -14,7 +14,7 @@ function SingleCardYT(props){
                 </div>
                 <h2 className="container__title__article"><a href="#">{props.titulo}</a></h2>
             </div>
-            <iframe width="100%" height="100%" src={props.video} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe width="100%" height="100%" src={props.video} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
         </div>
     )
 }
