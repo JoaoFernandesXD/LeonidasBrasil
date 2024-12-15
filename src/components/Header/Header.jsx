e07fd8357a04562dcc09dd535a77b5632626643f
João Fernandes <joaofernandesxd1@icloud.com>
2024-12-15 17:47:45 -0300

    ajustando topo site

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,18 +5,7 @@ import logo from '/src/assets/image/logo.png';
 function Header(){
     return(
         <header className='header'>
-            <div className='container__usuario_logado'>
-                 <div className='container__usuario_logado_botao'>
-                    <button class="botao">Botão 1</button>
-                    <button class="botao">Botão 2</button>
-
-                 </div>
-                 <img src={logo}></img>
-                 <div className='container__usuario_logado_botao'>
-                    <button class="botao">Botão 1</button>
-                    <button class="botao">Botão 2</button>
-                 </div>
-            </div> 
+                <img src={logo} alt="Logo Leonidas Brasil"></img>
         </header>
     )
 }
