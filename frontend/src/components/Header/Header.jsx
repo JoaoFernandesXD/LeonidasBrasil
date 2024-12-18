64cacf135f826199b33433295ceadf5233f8fc92
João Fernandes <joaofernandesxd1@icloud.com>
2024-12-17 23:50:37 -0300

    feat: adicionando button login no mobille no header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,12 +1,23 @@
+import React from 'react';
+
 import '../Header/Header.css';
+import ModalLogin from '../ModalLogin/ModalLogin';
 
 import logo from '/src/assets/image/logo.png';
 
+
 function Header(){
+    
+    const [open, setOpen] = React.useState(false);
+
     return(
+        <>
         <header className='header'>
                 <img src={logo} alt="Logo Leonidas Brasil"></img>
+                <button className="nav--btn-mobille" onClick={() => setOpen(true)}>Login</button>
         </header>
+         <ModalLogin open={open} onClose={() => setOpen(false)}></ModalLogin> 
+        </>
     )
 }
 
