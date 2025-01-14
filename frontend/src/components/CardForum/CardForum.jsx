ae7429215a7d265968ded5a9ba25778af6e019ad
João Fernandes <joaofernandesxd1@icloud.com>
2025-01-14 19:40:43 -0300

    feat: criando componete card forum e editando o container forum

---
+++ frontend/src/components/CardForum/CardForum.jsx
@@ -0,0 +1,13 @@
+import '../CardForum/CardForum.css';
+
+
+function CardForum(){
+    return(
+        <>
+        Olá sou um topico
+        </>
+    )
+}
+
+export default CardForum;
+
--- frontend/src/components/Forum/Forum.jsx
+++ frontend/src/components/Forum/Forum.jsx
@@ -1,11 +1,24 @@
 import '../Forum/Forum.css';
+import { RiSpeakFill } from "react-icons/ri";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 function Forum(){
     return(
         <>
            <div className='container__forum'>
                 <div className='row'></div>
-                Oi sou um forum :
+                <div className="container__forum__info">
+                <h2><RiSpeakFill /> Fórum</h2>
+                <div className="container__forum__pagination">
+                    {/* Setas com classes específicas */}
+                    <div className="arrow--pagination swiper-button-prev">
+                        <FaArrowLeft />
+                    </div>
+                    <div className="arrow--pagination swiper-button-next">
+                        <FaArrowRight />
+                    </div>
+                </div>
+            </div>
            </div>
         </>
     )
